8ff872db0b761a8554bb6631bd7e9f46ac1a37bd
jason <jason0526liu@gmail.com>
2023-11-06 18:03:34 +0800

    load effect,fix flash

--- header.js
+++ header.js
@@ -2,19 +2,31 @@
 window.onload = function(){
 
   let headerPlaceholder = document.getElementById("header-place");
-  let headerTemp = document.getElementById("header-temp");
+  let background = document.getElementById("background");
   fetch('./header.html',{mode: 'no-cors'})
   .then(response => response.text())
   .then(headerContent => {
       // 将header内容插入到占位符位置
       headerPlaceholder.innerHTML = headerContent;
+      background.style.display = "block";
+      background.style.opacity = "1";
   })
   .catch(error => {
       console.error('加载header内容时发生错误：', error);
   });
-  //var headerContent = '<header> <div class="header-all"> <div class = "header-background"> </div> <div class = "header-content"> <div class = "connect_link" id="about_me_button"> <p>About me</p> </div> <div class = "connect_link" id="leetcode_record_button"> <p>leetcode record</p> </div> <div class = "connect_link" id="game_development_button"> <p>Game Development</p> </div> <div class = "connect_link" id="web_development_button"> <p>Web Development</p> </div> <div class = "connect_link" id="contact_me_button"> <p>Contact me</p> </div> </div> </div> <div class = "header-space"> </div> </header>';
-  setTimeout(function(){
 
+  function waitForElementToAppear(elementId, callback) {
+    const intervalId = setInterval(function() {
+      const element = document.getElementById(elementId);
+      if (element) {
+        clearInterval(intervalId); // 停止轮询
+        callback(element);
+      }
+    }, 100); // 每秒检查一次
+  }
+  
+  // 用法示例
+  waitForElementToAppear('about_me_button', function(element) {
     let about_me_button = document.getElementById("about_me_button");
     about_me_button.addEventListener("click",function(event){
       console.log("index");
@@ -40,7 +52,14 @@ window.onload = function(){
     contact_me_button.addEventListener("click",function(event){
       window.location.href ="../../contact_me.html";
     })
-  },500)
+    // 在这里执行物件出现后的操作
+  });
+
+  //var headerContent = '<header> <div class="header-all"> <div class = "header-background"> </div> <div class = "header-content"> <div class = "connect_link" id="about_me_button"> <p>About me</p> </div> <div class = "connect_link" id="leetcode_record_button"> <p>leetcode record</p> </div> <div class = "connect_link" id="game_development_button"> <p>Game Development</p> </div> <div class = "connect_link" id="web_development_button"> <p>Web Development</p> </div> <div class = "connect_link" id="contact_me_button"> <p>Contact me</p> </div> </div> </div> <div class = "header-space"> </div> </header>';
+  setTimeout(function(){
+
+    
+  },300)
 
 }
 
